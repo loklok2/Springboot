e8c4238ca61e6e024ce44a8f2386fecdceb5df25
loklok2 <loklok2@naver.com>
2024-07-09 11:38:15 +0900

    13rdweek/day2/chap2

---
+++ board-spring-boot-starter/src/main/java/com/rubypaper/jdbc/config/BoardAutoConfiguration.java
@@ -0,0 +1,19 @@
+package com.rubypaper.jdbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.rubypaper.jdbc.util.JDBCConnectionManager;
+
+@Configuration
+public class BoardAutoConfiguration {
+	
+	@Bean
+	JDBCConnectionManager getJdbcConnectionManager() {
+		JDBCConnectionManager manager = new JDBCConnectionManager();
+		manager.setUrl("jdbc:mysql://localhost:3306/mission");
+		manager.setUsername("scott");
+		manager.setPassword("tiger");
+		return manager;
+	}
+}
